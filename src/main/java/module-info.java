ff4d1fec75613200831a4b9681ec949adb8d62a1
egolikov <panda_93a@mail.ru>
2023-07-23 16:17:02 +0600

    * remove

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.qaguru.gitconflict21 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.qaguru.gitconflict21 to javafx.fxml;
-    exports com.qaguru.gitconflict21;
-}
